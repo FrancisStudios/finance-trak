962a876865b2188af65156d888d3136b52156b14
Francis <vlfmap@gmail.com>
2026-08-22 09:39:29 +0200

    autologin

--- index.php
+++ index.php
@@ -22,17 +22,43 @@
 
     <?php
     require_once __DIR__ . "/src/modules/database-connection.module.php"; // This connects to db and establishes DB connection
-    include __DIR__."/src/login.php";
+    include __DIR__ . "/src/login.php";
 
     $pdo = DatabaseConnection::getConnection();
     ?>
 
     <span class="badge rounded-pill text-bg-light" id="bg-auth">©Francis Studios Softwares <a href="https://github.com/FrancisStudios/finance-trak">GitHub</a></span>
-    
+
     <script>
+        /**
+         * Autologin if user session object can be retrieved
+         * -------------------------------------------------
+         */
         let userSession = sessionStorage.getItem('ftses');
-        if(!userSession)
-        {
+        if (userSession && JSON.parse(userSession)) {
+
+            session = JSON.parse(userSession);
+
+            if (session.username && session.password) {
+                console.log("Attempting auto login...");
+
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", "./src/endpoints/login.endpoint.php", true);
+                xhr.setRequestHeader('Content-Type', 'application/json');
+                xhr.send(JSON.stringify({
+                    username: session.username,
+                    password: session.password
+                }));
+
+
+                xhr.onreadystatechange = function() {
+                    if (this.readyState != 4) return;
+                    if (this.status == 200) {
+                        let data = JSON.parse(this.responseText);
+                        window.location.href = './src/main.php';
+                    }
+                };
+            }
         }
     </script>
 </body>
